ef99849010b65a645a6171c9f42578d7826fba53
st-nikita <60624415+st-nikita@users.noreply.github.com>
2020-06-06 10:42:23 +0500

    fulldata

--- Common/FullData.cs
+++ Common/FullData.cs
@@ -6,6 +6,18 @@ namespace Common
 {
     public class FullData : InputData
     {
-
+        public string level { get; set; }          //уровень до которого распознали: house, street, city, subregion - мун район, region - область
+        public string country { get; set; }        //страна. есть не везде
+        public string region { get; set; }         //субьект
+        public string city { get; set; }           //город
+        public string settlement { get; set; }     //поселение
+        public string districtType { get; set; }   //тип муниципального образования Район / Поселение
+        public string districtName { get; set; }   //название района / поселения
+        public string street { get; set; }         //название улицы
+        public string houseNumber { get; set; }    //номер дома с литером: 12, 12А
+        public string houseStructure { get; set; } //строение 12 стр2
+        public string houseBody { get; set; }      //корпус 12 стр2 корп.1
+        public string flat { get; set; }           //квартира
+        public string office { get; set; }         //офис / помещение
     }
 }
